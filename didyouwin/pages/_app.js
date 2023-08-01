2ca85755744e04f18c55f176263df2408a651d76
ZINOO PARK <zinoo_park@ZINOOui-MacBookPro.local>
2023-08-02 06:48:47 +0900

    fix: UI

--- didyouwin/pages/_app.js
+++ didyouwin/pages/_app.js
@@ -2,10 +2,17 @@ import "../styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AuthProvider } from "../components/AuthProvider";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
       <div className="next-body">
         <Component {...pageProps} />
       </div>
