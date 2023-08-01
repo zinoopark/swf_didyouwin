9b9708d23b3d39f2208e673acc663a04b90e0fee
ZINOO PARK <zinoo_park@ZINOOui-MacBookPro.local>
2023-08-02 03:47:30 +0900

    add: chat

---
+++ didyouwin/pages/chat/index.js
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { nftList } from "../../competition";
+
+const typeList = ["All Chats", "Programming", "Art", "Climate", "Sports"];
+
+export default function Index() {
+  const router = useRouter();
+  const tab = router.query.tab;
+
+  return (
+    <div>
+      <div className="header">
+        <h2>Your Chats</h2>
+      </div>
+      <div className="sub-header">
+        <ul>
+          {typeList.map((type) => {
+            return (
+              <li className={`${type === tab ? "active" : ""}`} key={type}>
+                <Link href={`/chat?tab=${type}`}>{type}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="chat-list">
+        <ul>
+          {nftList.map((nft) => {
+            return (
+              <li key={nft.title}>
+                <Link href={`/chat/${nft.title}`}>
+                  <div className="chat">
+                    <div className="chat-img">
+                      <img src={nft.img_url} alt={nft.title} />
+                    </div>
+                    <div className="chat-title">
+                      <h3>{nft.title}</h3>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <style global jsx>{`
+        .chat-list li {
+          padding: 1rem;
+        }
+
+        .chat-img {
+          width: 5rem;
+          height: 5rem;
+          border-radius: 1rem;
+          overflow: hidden;
+        }
+        .chat-img img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+
+        .sub-header {
+          overflow-x: scroll;
+        }
+        .sub-header::-webkit-scrollbar {
+          width: 4px; /* 스크롤바의 너비 */
+        }
+        .sub-header ul {
+          width: max-content;
+          display: flex;
+          padding: 5px 2rem;
+          gap: 2rem;
+          font-size: 1.2rem;
+        }
+        .active {
+          color: var(--point-color);
+        }
+        body {
+          background-color: #fff;
+          min-height: 100dvh;
+          padding-block: 3rem;
+        }
+        .header h2 {
+          text-align: center;
+        }
+      `}</style>
+      `
+    </div>
+  );
+}
--- didyouwin/pages/my/[id].js
+++ didyouwin/pages/my/[id].js
@@ -1,130 +1,39 @@
 import { useRouter } from "next/router";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ConfettiExplosion from "react-confetti-explosion";
 
-const nftList = [
-  {
-    img_url: "https://seoulweb3festival.com/share/img/sub/img01_1_poster.jpg",
-    title: "Seoul Web 3.0 Festival 2023",
-    description: "Art Hall 2 & Design Hall, Dongdaemun Design Plaza(DDP)",
-    date: "2023-11-27",
-    rank: 1,
-    type: "Programming",
-  },
-  {
-    img_url:
-      "https://mediahub.seoul.go.kr/uploads/competition/2023/07/CCimglYRrmIIJlUVBafFqLBvMVxSxsXg.png",
-    title: "캠퍼스온에어-지구를 지켜라, 예스지! 환경영상공모전",
-    description: "2023 캠퍼스온에어-지구를 지키자, 예스지(ESG)! ",
-    date: "2023-08-07",
-    rank: 1,
-    type: "Climate",
-  },
-  {
-    img_url:
-      "https://mediahub.seoul.go.kr/uploads/competition/2023/07/tnGYckFWgvekepsNcPtLDzINtlYXXMRN.png",
-    title: "제16회 서울시 청소년 119안전뉴스 영상 경연대회",
-    description: "제16회 서울시 청소년 119안전뉴스 영상 경연대회",
-    date: "2023-07-24",
-    rank: 2,
-    type: "Art",
-  },
-  {
-    img_url:
-      "https://mediahub.seoul.go.kr/uploads/competition/2023/07/hRNtQDXdaQGQudJzGkWGnKiMjGbJhhGF.jpg",
-    title: "여름방학 서울런 학습수기·영상·웹툰 공모전",
-    description: "여름방학 서울런 학습수기·영상·웹툰 공모전",
-    date: "2023-09-24",
-    rank: 1,
-    type: "Art",
-  },
-  {
-    img_url:
-      "https://mediahub.seoul.go.kr/uploads/competition/2023/07/siicvKvTxMxOYYcrMcMdPbFzWgCnJxJf.png",
-    title: " 맛있는 아리수 숏폼, 웹툰 공모전",
-    description: "맛있는 아리수 숏폼, 웹툰 공모전",
-    date: "2023-02-11",
-    rank: 1,
-    type: "Art",
-  },
-  {
-    img_url:
-      "https://news.seoul.go.kr/culture/files/2023/07/64ae017df0fab2.75151665.jpg",
-    title: "제3회 서울유니버설디자인어워드",
-    description: "제3회 서울유니버설디자인어워드",
-    date: "2023-01-27",
-    rank: 1,
-    type: "Art",
-  },
-  {
-    img_url:
-      "https://womenmarathon.co.kr/data/file/board01_8012/thumb-238350739_LUWrSpMx_7c58b5b5c92941423d7d80a5853071f85b7b44c3_600x848.jpg",
-    title: "2023 서울여성마라톤",
-    description: "2023 서울여성마라톤",
-    date: "2023-11-27",
-    rank: 1,
-    type: "Sports",
-  },
-  {
-    img_url:
-      "https://mediahub.seoul.go.kr/uploads/competition/2023/06/PJBFKlcEdlgenLcNodzFjisYsmsvMZbY.png",
-    title: "2023 서울정원박람회 학생정원 아이디어 공모",
-    description: "월드컵 하늘공원 일대",
-    date: "2023-11-27",
-    rank: 1,
-    type: "Climate",
-  },
-  {
-    img_url:
-      "https://mediahub.seoul.go.kr/uploads/competition/2021/09/GMRZlAPkHMzDGcpBdRiIVDyKHOChBfqO.jpg",
-    title:
-      "2021 시민청 정책카페 '나와 닮은 반려식물 찾기 프로젝트 : 나의 비밀정원'",
-    description:
-      "2021 시민청 정책카페 '나와 닮은 반려식물 찾기 프로젝트 : 나의 비밀정원'",
-    date: "2023-11-27",
-    rank: 1,
-    type: "Climate",
-  },
-  {
-    img_url:
-      "https://mediahub.seoul.go.kr/uploads/competition/2021/12/ZlrIXUWnYraGexNRPciCmYjEcBeYdwsv.jpg",
-    title:
-      "청년과 함께하는 '서울시 에너지정보플랫폼 해커톤 대회' 시민평가단 모집",
-    description:
-      "청년과 함께하는 '서울시 에너지정보플랫폼 해커톤 대회' 시민평가단 모집",
-    date: "2022-01-27",
-    rank: 1,
-    type: "Programming",
-  },
-  {
-    img_url: "https://www.kchannel.kr/news/photo/202307/121215_116188_2216.png",
-    title: "서울 100Km",
-    description: "서울을 달리는 새로운 방법",
-    date: "2022-11-27",
-    rank: 1,
-    type: "Sports",
-  },
-  {
-    img_url:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5dYo1TwSramfQYTT7M2wK9LNnSyGvcHBoCdx2zRVUVvp2uR5xBIDXZPDa-m7Rig2Sqmk&usqp=CAU",
-    title: "2022 서울달리기",
-    description: "2022 서울달리기",
-    date: "2022-11-27",
-    rank: 1,
-    type: "Sports",
-  },
-];
+import { useEffect, useState } from "react";
+import { nftList } from "../../competition";
 
 export default function detail() {
   const router = useRouter();
   const id = router.query.id;
   const nft = nftList[id - 1];
+  const [isExplosion, setIsExplosion] = useState(false);
+  const [medalAnimate, setMedalAnimate] = useState(false);
 
   const goBack = () => {
     router.back();
   };
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setMedalAnimate(true);
+      setTimeout(() => {
+        setIsExplosion(true);
+      }, 400);
+    }
+  }, []);
+
   return (
     <div className="detail">
+      {isExplosion && (
+        <ConfettiExplosion
+          particleCount={400}
+          duration={2500}
+          force={1}
+        ></ConfettiExplosion>
+      )}
       <button
         onClick={goBack}
         style={{
@@ -137,10 +46,14 @@ export default function detail() {
         <ArrowBackIosNewIcon></ArrowBackIosNewIcon>
       </button>
       <div className="title">
-        <h5>{nft?.title}</h5>
+        <h3>{nft?.title}</h3>
       </div>
       <div className="img-div">
-        <img src={"/imgs/medal.png"} alt={nft?.title} />
+        <img
+          className={`${medalAnimate ? "medal-animation" : ""}`}
+          src={"/imgs/medal.png"}
+          alt={nft?.title}
+        />
       </div>
       <div className="date narrow">
         <p>{nft?.date}</p>
@@ -149,26 +62,37 @@ export default function detail() {
         <p>{nft?.description}</p>
       </div>
       <div className="narrow">
+        <p>Mass Adoption</p>
+      </div>
+      <div className="narrow rank">
         <p>{nft?.rank}st Place</p>
       </div>
       <div className="twitter">
         <p>Go get twitter badge.</p>
       </div>
       <style jsx>{`
+        .rank {
+          margin-top: 2rem;
+        }
+
         .title {
           padding-top: 8rem;
         }
-        .title h5 {
+        .title h3 {
           text-align: center;
+        }
+        .date {
+          margin-block: -1rem 2rem;
         }
         .img-div {
           width: 14rem;
           height: 14rem;
-          background: url(${nft?.img_url}) no-repeat center;
+          background: url(${nft?.img_url}) no-repeat center bottom;
           background-size: cover;
           border-radius: 1.5rem;
           backdrop-filter: grayscale(80%);
           margin: 2rem auto;
+          overflow: hidden;
         }
         .img-div img {
           display: block;
@@ -176,10 +100,16 @@ export default function detail() {
           height: 90%;
           margin: auto;
           object-fit: contain;
+          margin-top: 20rem;
+          transition: all 2.4s ease;
+        }
+        .medal-animation {
+          margin-top: 0 !important;
         }
         p {
           text-align: center;
-          margin-block: 2rem;
+          font-size: 1.2rem;
+          margin-block: 1rem;
         }
         .twitter {
           margin-top: 14rem;
